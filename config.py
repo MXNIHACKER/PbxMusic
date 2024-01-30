8595a9137ba817055d53a324b8aeffb217f1f96b
Mani <140153681+MXNIHACKER@users.noreply.github.com>
2024-01-30 17:00:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,9 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID"))
 
+#Put your bot username here
+BOT_USERNAME = getenv("New_Music_Test_Bot")
+
 # Get this value from  on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
 
