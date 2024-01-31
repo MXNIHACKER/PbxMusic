59815e3cd012df021244be843736b3cbe85e2535
Mani <140153681+MXNIHACKER@users.noreply.github.com>
2024-01-31 17:15:08 +0530

    Update sukhbass.py

--- DevilX/plugins/play/sukhbass.py
+++ DevilX/plugins/play/sukhbass.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pydub import AudioSegment
 import os
-from VIPMUSIC import app
+from DevilX import app
 
 
 @app.on_message(filters.command("bass"))
